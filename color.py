bdaff5fba5bca09993ef9362761c0d32545c6bf7
Yogeshwaran <132633979+yogesh932003@users.noreply.github.com>
2023-05-06 20:52:48 +0530

    Add files via upload

---
+++ color.py
@@ -0,0 +1,9 @@
+color_1 = "gray95"
+color_2 = "yellow","green"
+color_3 = "black"
+color_4 = "white"
+
+font_1 = "times new roman"
+font_2 = "helvetica"
+
+#25
---
+++ database.py
@@ -0,0 +1,5 @@
+# User database
+host = 'localhost'
+user = 'root'
+password = '5266266444@My'
+database = 'student_management'
